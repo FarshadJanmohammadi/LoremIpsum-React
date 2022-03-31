c9a8b055cdc1772fc66baf9e03feb0d983eaae6e
Farshad <FarshadJanmohammadi@gmail.com>
2022-03-31 15:23:24 +0430

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newText = data;
+    const newCount = count > 0 ? count : 1;
+    const newTextFiltered = newText.slice(0, newCount);
+    setText(newTextFiltered);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraf:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          genarate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
   );
 }
 
